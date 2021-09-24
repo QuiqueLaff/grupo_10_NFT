8f8d0ed237dd55220c0f01bcf560eaac115e33ea
Nicolás Albarracín <albanico2002@gmail.com>
2021-09-24 11:13:00 -0300

    modificacion de register

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
