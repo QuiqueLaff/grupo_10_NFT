5c7962fc6373947fd5291da25ce70cf58b5a8dbd
aleomg <alejandro.faundes1@gmail.com>
2021-09-30 01:39:54 -0300

    Se agrega mockup de Carrito y de Producto

--- app.js
+++ app.js
@@ -21,3 +21,7 @@ app.get('/product', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/login.html'));
 })
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/cart.html'));
+})
