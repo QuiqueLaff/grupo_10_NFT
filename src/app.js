90bbf8e2a4ba55148a496fd5029ff891423521c5
Quiquelaff <enriquelaffranconi@gmail.com>
2021-10-27 10:48:06 -0300

    Se incorporo session

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
+const sesion = require('express-session')
 
 
 /* view engine */
@@ -14,6 +15,7 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
 app.use (express.json());
+app.use(sesion({secret: 'Cuidadito'}))
 
 /* Routes */
 
