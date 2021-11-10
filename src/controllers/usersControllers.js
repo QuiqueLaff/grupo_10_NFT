7f43820f7b67948ed805a7318a02d49c99e569f0
aleomg <alejandro.faundes1@gmail.com>
2021-11-10 20:25:07 -0300

    Se implementa registro de usuarios

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,10 +1,58 @@
 const path = require('path');
+const fs = require('fs');
+const res = require('express/lib/response');
+const bcrypt = require('bcryptjs');
+
+const saltRounds = 10;
+
+const jsonUsers = fs.readFileSync(path.resolve(__dirname, '../db/users.json'), 'utf-8');
+const users = JSON.parse(jsonUsers); 
+
+
+
+
+const nuevoUserId = () => {
+    let ultimo = 0;
+    users.forEach(user => {
+        if (user.id > ultimo) {
+            ultimo = user.id;
+        }
+    });
+    return ultimo + 1;
+}
 
 module.exports = {
-    register: (req, res) => {
-        res.render(path.resolve('src/views/register.ejs'));  
-    },
-    login: (req, res) => {
-        res.render(path.resolve('src/views/login.ejs'));
+    home: (req, res) => {
+
+        res.render('users');
     }
+    ,
+    register: (req, res) => {
+        res.render('register');  
+    },
+
+    store (req, res) {
+        let userImage = req.file.filename
+
+        let newUser = {
+            id: nuevoUserId(),
+            ...req.body,
+            password: bcrypt.hashSync(req.body.password, 10),
+             image: userImage || 'default-image2.png',
+        }
+     
+        users.push(newUser);
+    
+        let jsonUsers = JSON.stringify(users, null, 4);
+        fs.writeFileSync(path.resolve(__dirname, '../db/users.json'), jsonUsers);
+    
+        res.redirect('/');
+    },
+    
+        login: (req, res) => {
+            res.render('login');
+        },
 }
+
+
+// if (bcrypt.compareSync(req.body.password)
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require ('multer');
 const usersControllers = require('../controllers/usersControllers');
 
+
+// MULTER
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null, path.resolve(__dirname, '../../public/images/users'))
+    },
+    filename: function(req,file,cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({storage: storage})
+
+
+// Todos los usuarios 
+router.get('/', usersControllers.home);
+
+
+// Registro 
+
 router.get('/register', usersControllers.register);
+
+router.post('/register', upload.single('userImage'), usersControllers.store);
+
+
+// Login
 
 router.get('/login', usersControllers.login)
 
