312fa8fcd52f2f766761b10e43c44a2e2b3ff468
aleomg <alejandro.faundes1@gmail.com>
2021-12-23 21:08:11 -0300

    Se inicia CRUD

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "nft_db",
+    "database": "",
     "port":3306,
     "host": "127.0.0.1",
     "dialect": "mysql"
