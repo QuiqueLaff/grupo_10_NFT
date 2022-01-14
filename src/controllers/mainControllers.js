40b916cef126ddd2c85f3decd7a5f58470167fe5
Juan Salguero <salguerojuanfrancisco@gmail.com>
2022-01-13 21:18:24 -0300

    randomizador de productos en el home SIN REPETIR

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -4,10 +4,24 @@ const db = require("../database/models")
 
 module.exports = {
     home: (req, res) => {
+        
         db.Products.findAll()
             .then((product)=>{
-                console.log(product);
-                return res.render("home",{product})
+                let myArray = []
+                while(myArray.length < 3){
+                    var randomIndex = Math.floor(Math.random()*product.length);
+                    var checked = false;
+                    for(var i=0;i<myArray.length;i++){
+                        if(myArray [i] == randomIndex){
+                            checked = true;
+                            break;
+                        }
+                    }
+                    if(!checked){
+                        myArray[myArray.length] = randomIndex;
+                    }
+                    }
+                    return res.render("home",{product,myArray})
             }).catch((error)=>{
                 return res.send(error)
             })
